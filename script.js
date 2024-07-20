f8e7c38226a75d818a097d602bcd80d79b55ed31
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-07-20 23:15:07 +0100

    edit script file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
-    form.addEventListener('submit', (event)=>{
+    form.addEventListener('submit', function(event){
         event.preventDefault();
         const username = document.getElementById('username').value.trim();
         const email = document.getElementById('email').value.trim();
